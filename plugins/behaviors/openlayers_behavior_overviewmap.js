583e26de0e08a3203af4396728344e2e8d7dd5b9
basvredeling <basvredeling@207078.no-reply.drupal.org>
2014-11-26 10:46:59 +0100

    Issue #2381177 by basvredeling: Add extra params to overview map behavior

--- plugins/behaviors/openlayers_behavior_overviewmap.js
+++ plugins/behaviors/openlayers_behavior_overviewmap.js
@@ -7,5 +7,6 @@
  * Overview Map Behavior. Implements the Overview Control Map.
  */
 Drupal.openlayers.addBehavior('openlayers_behavior_overviewmap', function (data, options) {
+    options.maximized = JSON.parse(options.maximized);
     Drupal.openlayers.addControl(data.openlayers, 'OverviewMap', options);
 });
